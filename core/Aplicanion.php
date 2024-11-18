065a475395cfecfab7e5ddb54fe4132fc231c863
Aleksei-chance <aleksei.chance@gmail.com>
2024-11-18 23:33:19 +0300

    Request class

--- core/Aplicanion.php
+++ core/Aplicanion.php
@@ -4,5 +4,13 @@ namespace core;
 
 class Aplicanion
 {
+    protected string $uri;
+    public Request $request;
+
+    public function __construct()
+    {
+        $this->uri = $_SERVER['QUERY_STRING'];
+        $this->request = new Request($this->uri);
+    }
 
 }
